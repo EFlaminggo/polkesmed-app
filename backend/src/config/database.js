9658f585363a1401189bcfed1e4e4dafb7f8b6a0
sherwin-77 <sherwinak1@gmail.com>
2023-12-14 23:19:20 +0700

    Flex db port for personal

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 const db_pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
